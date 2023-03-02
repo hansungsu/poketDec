900f75060cdf9cb76ef6696043db12a29ed90573
HanSungSoo <thepub03@gmail.com>
2023-03-02 21:20:34 +0900

    first init

---
+++ src/routes/mypage/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+import Info from './Info.svelte';
+</script>
+<svelte:head>
+	<title>Mypage</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>마이페이지 테스트</h1>
+	<Info />
+</div>
